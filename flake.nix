2950ff778e12bd63af93597b85cd6f64f362768a
George Thomas <georgefsthomas@gmail.com>
2025-12-02 16:42:44 +0000

    Add simple Rust Crane project
    The Nix code is taken from the Crane docs, and adapted for merging with the existing Haskell.Nix flake. The other files are from `cargo init`, with minor modifications, and `cargo generate-lockfile`.
    
    The default dev shell contains tools for both languages.
    
    We can now run `nix build .#rust` and `nix build .#haskell`, where the latter was previously `nix build .#aoc:exe:aoc`.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,8 @@
   inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
   inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+  inputs.crane.url = "github:ipetkov/crane";
+  outputs = { self, nixpkgs, flake-utils, haskellNix, crane }:
     flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
       let
         overlays = [
@@ -17,7 +18,19 @@
         ];
         pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
         haskell = pkgs.hixProject.flake { };
+        rust = crane.mkLib pkgs;
       in
-      haskell
+      {
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [
+            haskell.devShells.default
+            (rust.devShell { })
+          ];
+        };
+        packages = {
+          haskell = haskell.packages."aoc:exe:aoc";
+          rust = rust.buildPackage { src = rust.cleanCargoSource ./.; };
+        };
+      }
     );
 }
