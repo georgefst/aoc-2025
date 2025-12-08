5539a4bd05bf8b6b95ab8680d515bd4c31d72eb2
George Thomas <georgefsthomas@gmail.com>
2025-12-08 15:52:20 +0000

    Solve day 6 part 1

--- haskell/Puzzles/Day6.hs
+++ haskell/Puzzles/Day6.hs
@@ -2,14 +2,32 @@ module Puzzles.Day6 (puzzle) where
 
 import Pre
 
+import Data.Text.Lazy qualified as TL
+
 puzzle :: Puzzle
 puzzle =
     Puzzle
         { number = 6
-        , parser = pure ()
+        , parser = do
+            ints <- (hspace *> (decimal `sepBy1` hspace1)) `sepEndBy1` newline
+            ops <- ((single '*' $> Multiply) <|> (single '+' $> Add)) `sepEndBy` hspace1
+            void newline
+            pure (ops, transpose ints)
         , parts =
-            [ \() ->
-                "TODO"
+            [ TL.show
+                . sum
+                . uncurry (zipWith \op -> foldl' (apply op) (unit op))
             ]
         , extraTests = mempty
         }
+
+data Op = Add | Multiply
+    deriving (Eq, Ord, Show, Enum, Bounded)
+apply :: Op -> Int -> Int -> Int
+apply = \case
+    Add -> (+)
+    Multiply -> (*)
+unit :: Op -> Int
+unit = \case
+    Add -> 0
+    Multiply -> 1
